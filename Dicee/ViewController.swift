045ecfb15260f54f4636c92e7d3d7e31c813352c
[Arusey] <[kevinkibetw7@gmail.com]>
2019-09-10 12:32:00 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Kevin Lagat on 10/09/2019.
+//  Copyright © 2019 Kevin Lagat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
