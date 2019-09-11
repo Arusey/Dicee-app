b7f29e900ca44c2defabb4c01f5b909aa1ca6e44
[Arusey] <[kevinkibetw7@gmail.com]>
2019-09-11 14:44:03 +0300

    basic starting code for the dicee app

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -4,17 +4,34 @@
 //
 //  Created by Kevin Lagat on 10/09/2019.
 //  Copyright © 2019 Kevin Lagat. All rights reserved.
-//
+///Users/kevinlagat/Desktop/Projects/Dicee/Dicee/ViewController.swift
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //how can i push this freaking piec of code!!1
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+        print(randomDiceIndex2)
+        
+    }
+    
 }
 
